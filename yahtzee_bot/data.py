29206e8b3114e4c9ea5d177bd90b2e6ba0862033
nTollot <nicolas.tollot@gmail.com>
2022-11-26 19:32:06 +0100

    Fixes major bugs to the available moves function

--- yahtzee_bot/data.py
+++ yahtzee_bot/data.py
@@ -40,27 +40,27 @@ class CollectSampleExperiments():
         self.model_dice_1 = model_dice_1
         self.model_dice_2 = model_dice_2
         self.model_box = model_box
-        self.history_model_dice_1 = [[] for _ in range(n_games)]
-        self.history_model_dice_2 = [[] for _ in range(n_games)]
-        self.history_model_box = [[] for _ in range(n_games)]
 
     def initialize(self):
         self.value_box = np.zeros(
-            (self.n_games, self.box_size), dtype=np.float32)
+            (self.n_games, self.n_boxes), dtype=np.float32)
         self.is_box_checked = np.zeros(
-            (self.n_games, self.box_size), dtype=np.float32)
+            (self.n_games, self.n_boxes), dtype=np.int32)
         self.dice = np.zeros(
-            (self.n_games, self.n_dices*self.dice_max_value), dtype=np.float32)
+            (self.n_games, self.n_dices*self.dice_max_value), dtype=np.int32)
+        self.history_model_dice_1 = [[] for _ in range(self.n_games)]
+        self.history_model_dice_2 = [[] for _ in range(self.n_games)]
+        self.history_model_box = [[] for _ in range(self.n_games)]
 
     def available_moves(self):
         # Based on the rules on https://www.hasbro.com/common/instruct/yahtzee.pdf
         # without the wadzee bonus
         available_boxes = np.zeros(
-            (self.n_games, self.n_boxes), dtype=np.float32)
+            (self.n_games, self.n_boxes), dtype=np.int32)
 
-        n_identical_dices = np.zeros((self.n_games, 6), dtype=np.float32)
+        n_identical_dices = np.zeros((self.n_games, 6), dtype=np.int32)
         for i in range(self.dice_max_value):
-            n_identical_dices[:i] = np.sum(
+            n_identical_dices[:, i] = np.sum(
                 self.dice[:, i+np.arange(0, self.n_dices)*self.dice_max_value], axis=1)
 
         # Upper Section
@@ -93,15 +93,15 @@ class CollectSampleExperiments():
 
         # Full House
         available_boxes[:, 8] = (n_identical_dices == 3).any(
-            axis=1) and (n_identical_dices == 2).any(axis=1)
+            axis=1) * (n_identical_dices == 2).any(axis=1)
 
         # Small Straight
-        available_boxes[:, 9] = (n_identical_dices[:, :3] > 0).all(axis=1) or (
-            n_identical_dices[:, 1:4] > 0).all(axis=1) or (n_identical_dices[:, 2:] > 0).all(axis=1)
+        available_boxes[:, 9] = np.clip((n_identical_dices[:, :3] > 0).all(axis=1) + (
+            n_identical_dices[:, 1:4] > 0).all(axis=1) + (n_identical_dices[:, 2:] > 0).all(axis=1), 0, 1)
 
         # Large Straight
         available_boxes[:, 10] = (n_identical_dices[:, :3] > 0).all(
-            axis=1) or (n_identical_dices[:, 2:] > 0).all(axis=1)
+            axis=1) + (n_identical_dices[:, 2:] > 0).all(axis=1)
 
         # Chance
         available_boxes[:, 11] = 1
@@ -111,27 +111,27 @@ class CollectSampleExperiments():
 
         available_boxes_mask = available_boxes*(1-self.is_box_checked)
 
-        # If the player has no available box
-        available_boxes_mask[(available_boxes_mask == 0).all(
-            axis=1)*(1-available_boxes[:, 12])] = 1-self.is_box_checked
+        # If the player has no available box and no Yahtzee
+
+        no_available_box = (available_boxes[:, 12]==0)*(available_boxes_mask == 0).all(axis=1)
+        available_boxes_mask[no_available_box] = (1-self.is_box_checked)[no_available_box]
 
         # First case : Joker
-        # In this case, the boxes [Full House, Small Straight, Large Straight] are added to available_boxes
-        yahtzee_indexes_joker = available_boxes_mask[available_boxes[:,
-                                                                     12]*self.is_box_checked[:, 12]]*(self.value_box[:, 12] == 0)
+        # In this case, the boxes [Full House, Small Straight, Large Straight] are added to available_boxes,
+        # only if the upper section of the value of the dice is not empty
+        yahtzee_indexes_joker = available_boxes[((available_boxes_mask == 0).all(axis=1)*available_boxes[:, 12]*self.is_box_checked[:, 12]*(self.value_box[:, 12] == 0)).astype(bool)]
         available_boxes[yahtzee_indexes_joker, 8:10] = 1
 
         # Second case : Yahtzee Bonus
-        yahtzee_indexes_bonus = available_boxes_mask[available_boxes[:,
-                                                                     12]*self.is_box_checked[:, 12]]*(self.value_box[:, 12] > 0)
+        yahtzee_indexes_bonus = available_boxes[(available_boxes[:, 12]*self.is_box_checked[:, 12]*(self.value_box[:, 12] > 0)).astype(bool)]
         available_boxes[yahtzee_indexes_bonus, 13] += 1
         available_boxes[yahtzee_indexes_bonus, :13] = 1
 
         # We reapply mask
         available_boxes_mask = available_boxes*(1-self.is_box_checked)
-        available_boxes_mask[(available_boxes_mask == 0).all(
-            axis=1)*(1-available_boxes[:, 12])] = 1-self.is_box_checked
-        
+        no_available_box = (available_boxes[:, 12]==0)*(available_boxes_mask == 0).all(axis=1)
+        available_boxes_mask[no_available_box] = (1-self.is_box_checked)[no_available_box]
+
         return available_boxes_mask
 
     def determine_intermediate_reward(self):
