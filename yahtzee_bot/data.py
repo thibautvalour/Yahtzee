f40fc70f7f17023a645e2ddfa559704d9171595d
nTollot <nicolas.tollot@gmail.com>
2022-11-27 19:24:18 +0100

    Adding a draft function that determines the reward and changes the function that diplay the available moves to indicate whether the checked box earns points

--- yahtzee_bot/data.py
+++ yahtzee_bot/data.py
@@ -5,6 +5,8 @@ class CollectSampleExperiments():
     """
     Generates samples of parts played by the actions of the model
 
+    Based on the rules on https://www.hasbro.com/common/instruct/yahtzee.pdf
+
     Parameters
     ----------
     n_games : int
@@ -53,89 +55,190 @@ class CollectSampleExperiments():
         self.history_model_box = [[] for _ in range(self.n_games)]
 
     def available_moves(self):
-        # Based on the rules on https://www.hasbro.com/common/instruct/yahtzee.pdf
-        # without the wadzee bonus
-        available_boxes = np.zeros(
+        """
+        Generates an array of every possible move available, and an array determining if any box has been succefully reached
+
+
+        - Determines which boxes can be checked
+
+        - Determines which games have no real checkboxes and no Yahtzee
+
+        - Determines which games have a Yahtzee or Joker bonus
+
+        - Determines, for games where there is a Yahtzee bonus or a Joker, which boxes are checkable
+
+
+        Returns
+        ----------
+        is_any_box_reached : array
+            1 if a box has been reached, 0 else
+
+        available_boxes : array
+            Every box that the bot can check
+        """
+        self.available_boxes = np.zeros(
             (self.n_games, self.n_boxes), dtype=np.int32)
 
-        n_identical_dices = np.zeros((self.n_games, 6), dtype=np.int32)
+        self.n_identical_dices = np.zeros((self.n_games, 6), dtype=np.int32)
         for i in range(self.dice_max_value):
-            n_identical_dices[:, i] = np.sum(
+            self.n_identical_dices[:, i] = np.sum(
                 self.dice[:, i+np.arange(0, self.n_dices)*self.dice_max_value], axis=1)
+
+        # Determines which boxes can be checked
 
         # Upper Section
 
         # Aces
-        available_boxes[:, 0] = (n_identical_dices[:, 0] > 0)
+        self.available_boxes[:, 0] = (self.n_identical_dices[:, 0] > 0)
 
         # Twos
-        available_boxes[:, 1] = (n_identical_dices[:, 1] > 0)
+        self.available_boxes[:, 1] = (self.n_identical_dices[:, 1] > 0)
 
         # Threes
-        available_boxes[:, 2] = (n_identical_dices[:, 2] > 0)
+        self.available_boxes[:, 2] = (self.n_identical_dices[:, 2] > 0)
 
         # Fours
-        available_boxes[:, 3] = (n_identical_dices[:, 3] > 0)
+        self.available_boxes[:, 3] = (self.n_identical_dices[:, 3] > 0)
 
         # Fives
-        available_boxes[:, 4] = (n_identical_dices[:, 4] > 0)
+        self.available_boxes[:, 4] = (self.n_identical_dices[:, 4] > 0)
 
         # Sixes
-        available_boxes[:, 5] = (n_identical_dices[:, 5] > 0)
+        self.available_boxes[:, 5] = (self.n_identical_dices[:, 5] > 0)
+
+        # Lower Section
+
+        # 3 of a kind
+        self.available_boxes[:, 6] = (self.n_identical_dices > 2).any(axis=1)
+
+        # 4 of a kind
+        self.available_boxes[:, 7] = (self.n_identical_dices > 3).any(axis=1)
+
+        # Full House
+        self.available_boxes[:, 8] = (self.n_identical_dices == 3).any(
+            axis=1) * (self.n_identical_dices == 2).any(axis=1)
+
+        # Small Straight
+        self.available_boxes[:, 9] = np.clip((self.n_identical_dices[:, :3] > 0).all(axis=1) + (
+            self.n_identical_dices[:, 1:4] > 0).all(axis=1) + (self.n_identical_dices[:, 2:] > 0).all(axis=1), 0, 1)
+
+        # Large Straight
+        self.available_boxes[:, 10] = (self.n_identical_dices[:, :3] > 0).all(
+            axis=1) + (self.n_identical_dices[:, 2:] > 0).all(axis=1)
+
+        # Chance
+        self.available_boxes[:, 11] = 1
+
+        # Yahtzee
+        self.available_boxes[:, 12] = (self.n_identical_dices == 5).any(axis=1)
+        # Determines which games have no real checkboxes and no Yahtzee
+
+        self.is_any_box_reached = np.ones(self.n_games, dtype=np.int32)
+
+        available_boxes_mask = self.available_boxes*(1-self.is_box_checked)
+        no_available_box = (
+            self.available_boxes[:, 12] == 0)*(available_boxes_mask == 0).all(axis=1)
+
+        self.is_any_box_reached[no_available_box] = 0
+
+        available_boxes_mask[no_available_box] = (
+            1-self.is_box_checked)[no_available_box]
+
+        # Determines which games have a Yahtzee or Joker bonus
+        # In this case, the boxes [Full House, Small Straight, Large Straight] are added to available_boxes,
+        # only if the upper section of the value of the dice is not empty
+
+        self.is_yahtzee_bonus = np.zeros(self.n_games, dtype=np.int32)
+        self.is_joker = np.zeros(self.n_games, dtype=np.int32)
+
+        self.is_yahtzee_bonus[(self.available_boxes[:, 12]*self.is_box_checked[:, 12]
+                               * (self.value_box[:, 12] > 0)).astype(bool)] = 1
+        self.is_joker[(self.available_boxes[:, 12] *
+                       self.is_box_checked[:, 12]).astype(bool)] = 1
+
+        # Add self.is_yahtzee bonus to the reward function
+
+        # Then, we modify the available_boxes array according to the rules
+        # You need to score in the appropriate box in the Upper Section
+        # If not possible, you need to score any available box in the Lower Section box
+        # If not possible, you need to score any available box, but no point can be obtain
+
+        is_first_joker = np.zeros(self.n_games, dtype=bool)
+        is_second_joker = np.zeros(self.n_games, dtype=bool)
+        is_third_joker = np.zeros(self.n_games, dtype=bool)
+
+        is_first_joker[(self.is_box_checked[self.is_joker, np.argmax(
+            self.dice[self.is_joker], axis=1)]).all()] = 1
+        is_second_joker[(self.is_box_checked[self.is_joker -
+                                             is_first_joker, 6:12]).all()] = 1
+        is_third_joker = (self.is_joker - is_second_joker -
+                          is_first_joker).astype(bool)
+
+        # Determines, for games where there is a Yahtzee bonus or a Joker, which boxes are checkable
+
+        self.is_any_box_reached[is_third_joker] = 1
+
+        self.available_boxes[is_first_joker, np.argmax(
+            self.dice[is_first_joker], axis=1)] = 1
+        self.available_boxes[(1-self.is_box_checked)
+                             [is_second_joker, 6:12]] = 1
+        self.available_boxes[(1-self.is_box_checked)[is_third_joker]] = 1
+
+    def determine_intermediate_reward(self):
+        reward = np.zeros(self.n_games, dtype=np.float32)
 
         # Upper Section
 
+        # Aces
+        reward[self.decision == 0] = self.n_identical_dices[self.decision == 0, 0]
+
+        # Twos
+        reward[self.decision == 1] = self.n_identical_dices[self.decision == 1, 1]*2
+
+        # Threes
+        reward[self.decision == 2] = self.n_identical_dices[self.decision == 2, 2]*3
+
+        # Fours
+        reward[self.decision == 3] = self.n_identical_dices[self.decision == 3, 3]*4
+
+        # Fives
+        reward[self.decision == 4] = self.n_identical_dices[self.decision == 4, 4]*5
+
+        # Sixes
+        reward[self.decision == 5] = self.n_identical_dices[self.decision == 5, 5]*6
+
+        # Lower Section
+
         # 3 of a kind
-        available_boxes[:, 6] = (n_identical_dices > 2).any(axis=1)
+        reward[self.decision == 6] = np.sum(
+            self.n_identical_dices[self.decision == 6]*np.arange(1, 7), axis=1)
 
         # 4 of a kind
-        available_boxes[:, 7] = (n_identical_dices > 3).any(axis=1)
+        reward[self.decision == 7] = np.sum(
+            self.n_identical_dices[self.decision == 7]*np.arange(1, 7), axis=1)
 
         # Full House
-        available_boxes[:, 8] = (n_identical_dices == 3).any(
-            axis=1) * (n_identical_dices == 2).any(axis=1)
+        reward[self.decision == 8] = 25
 
         # Small Straight
-        available_boxes[:, 9] = np.clip((n_identical_dices[:, :3] > 0).all(axis=1) + (
-            n_identical_dices[:, 1:4] > 0).all(axis=1) + (n_identical_dices[:, 2:] > 0).all(axis=1), 0, 1)
+        reward[self.decision == 9] = 30
 
         # Large Straight
-        available_boxes[:, 10] = (n_identical_dices[:, :3] > 0).all(
-            axis=1) + (n_identical_dices[:, 2:] > 0).all(axis=1)
+        reward[self.decision == 10] = 40
 
         # Chance
-        available_boxes[:, 11] = 1
+        reward[self.decision == 11] = np.sum(
+            self.n_identical_dices[self.decision == 11]*np.arange(1, 7), axis=1)
 
         # Yahtzee
-        available_boxes[:, 12] = (n_identical_dices == 5).any(axis=1)
+        reward[self.decision == 12] = 50
 
-        available_boxes_mask = available_boxes*(1-self.is_box_checked)
+        # Yahtzee bonus
+        reward[self.decision == 13] = 100
 
-        # If the player has no available box and no Yahtzee
+        self.value_box[np.arange(0, self.n_games), self.decision] += reward
 
-        no_available_box = (available_boxes[:, 12]==0)*(available_boxes_mask == 0).all(axis=1)
-        available_boxes_mask[no_available_box] = (1-self.is_box_checked)[no_available_box]
-
-        # First case : Joker
-        # In this case, the boxes [Full House, Small Straight, Large Straight] are added to available_boxes,
-        # only if the upper section of the value of the dice is not empty
-        yahtzee_indexes_joker = available_boxes[((available_boxes_mask == 0).all(axis=1)*available_boxes[:, 12]*self.is_box_checked[:, 12]*(self.value_box[:, 12] == 0)).astype(bool)]
-        available_boxes[yahtzee_indexes_joker, 8:10] = 1
-
-        # Second case : Yahtzee Bonus
-        yahtzee_indexes_bonus = available_boxes[(available_boxes[:, 12]*self.is_box_checked[:, 12]*(self.value_box[:, 12] > 0)).astype(bool)]
-        available_boxes[yahtzee_indexes_bonus, 13] += 1
-        available_boxes[yahtzee_indexes_bonus, :13] = 1
-
-        # We reapply mask
-        available_boxes_mask = available_boxes*(1-self.is_box_checked)
-        no_available_box = (available_boxes[:, 12]==0)*(available_boxes_mask == 0).all(axis=1)
-        available_boxes_mask[no_available_box] = (1-self.is_box_checked)[no_available_box]
-
-        return available_boxes_mask
-
-    def determine_intermediate_reward(self):
-        pass
+        return reward
 
     def determine_final_reward(self):
         pass
